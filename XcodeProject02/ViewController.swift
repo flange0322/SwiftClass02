50d067ee7784181e55648003d303d62d51fa1675
Capoo <flange0322@gmail.com>
2019-04-15 11:45:52 +0800

    4/15 class03 update

--- XcodeProject02/ViewController.swift
+++ XcodeProject02/ViewController.swift
@@ -1,18 +1,41 @@
-//
-//  ViewController.swift
-//  XcodeProject02
-//
-//  Created by L20102-02 on 2019/4/8.
-//  Copyright © 2019年 L20102-02. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.black
+        
+        let a = add(a: -2, b: -5)
+        
+        if a > 0{
+            print("a 值為：\( a )")
+        }
+        else{
+            print("a 值小於０：\( a )")
+        }
+    }
+    
+    @IBAction func showMessage(sender: UIButton){
+        
+        let dictionary = ["Button-Test1": "Hello","Button-Test2": "World","Button-Test3": "to","Button-Test4": "you"]
+        let selectedButton = sender
+        
+        if let wordToLookup = selectedButton.titleLabel?.text{
+            
+            let meaning = dictionary[wordToLookup]
+            
+            let alertController = UIAlertController(title: "Meaning"
+                , message: meaning, preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alertController,animated: true,completion: nil)
+        }
+    }
+    
+    func add(a: Int, b: Int) -> Int{
+        return a + b
     }
 }
-
